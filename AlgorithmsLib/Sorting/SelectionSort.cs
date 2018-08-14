3f8e4e75918d55b43bb46afc0831a520577b4f25
Kenneth Lucero <Kenneth@KennethLucero.com>
2018-08-14 14:20:28 -0700

    added library for algorithms. Sorting section added. bubble and insert sort implemented, merge, quick, selection, and tree sort stubbed.

---
+++ AlgorithmsLib/Sorting/SelectionSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLib.Sorting
+{
+    public class SelectionSort<T> where T : IComparable
+    {
+        public static T[] Sort(T[] array)
+        {
+            return array;
+        }
+    }
+}
